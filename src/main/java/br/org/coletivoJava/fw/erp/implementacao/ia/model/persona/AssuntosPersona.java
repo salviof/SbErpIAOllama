9bcdb5b7f5c52d09874a1aad06633b655bf97860
Samuel <samuelfilipevianadois@gmail.com>
2025-06-04 17:17:55 -0300

    adiciona nova logica de assuntos

---
+++ src/main/java/br/org/coletivoJava/fw/erp/implementacao/ia/model/persona/AssuntosPersona.java
@@ -0,0 +1,53 @@
+package br.org.coletivoJava.fw.erp.implementacao.ia.model.persona;
+
+import com.super_bits.modulosSB.Persistencia.registro.persistidos.EntidadeSimples;
+import com.super_bits.modulosSB.SBCore.modulos.objetos.InfoCampos.anotacoes.InfoCampo;
+import com.super_bits.modulosSB.SBCore.modulos.objetos.InfoCampos.anotacoes.InfoObjetoSB;
+import com.super_bits.modulosSB.SBCore.modulos.objetos.InfoCampos.campo.FabTipoAtributoObjeto;
+import net.bytebuddy.utility.nullability.MaybeNull;
+
+import javax.persistence.*;
+
+@Entity
+@InfoObjetoSB(plural = "Assuntos de persona",tags = "Assunto de persona")
+public class AssuntosPersona extends EntidadeSimples {
+    @Id
+    @InfoCampo(tipo =FabTipoAtributoObjeto.ID)
+    private Long id;
+    @InfoCampo(tipo = FabTipoAtributoObjeto.NOME)
+    @Column(length = 100)
+    private String assunto;
+
+    @Column(length = 5000)
+    private String descricao;
+
+
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAssunto() {
+        return assunto;
+    }
+
+    public void setAssunto(String assunto) {
+        this.assunto = assunto;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+
+}
