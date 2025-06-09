d8593466e3bdb210ef54a9acbbfd9c00f5035fb2
Samuel <samuelfilipevianadois@gmail.com>
2025-06-09 16:54:10 -0300

    adiciona tratamento de erro personalizado

---
+++ src/main/java/br/org/coletivoJava/fw/erp/implementacao/excecoes/ErroOllamaERP.java
@@ -0,0 +1,7 @@
+package br.org.coletivoJava.fw.erp.implementacao.excecoes;
+
+public class ErroOllamaERP extends Exception {
+    public ErroOllamaERP(String message) {
+        super(message);
+    }
+}
