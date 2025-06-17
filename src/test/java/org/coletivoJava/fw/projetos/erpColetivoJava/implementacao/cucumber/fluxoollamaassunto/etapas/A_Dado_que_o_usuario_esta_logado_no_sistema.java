09c9d465982d96559b88679b5e10bdc0c62bc874
Samuel <samuelfilipevianadois@gmail.com>
2025-06-17 15:00:25 -0300

    adiciona docker compose

--- src/test/java/org/coletivoJava/fw/projetos/erpColetivoJava/implementacao/cucumber/fluxoollamaassunto/etapas/A_Dado_que_o_usuario_esta_logado_no_sistema.java
+++ src/test/java/org/coletivoJava/fw/projetos/erpColetivoJava/implementacao/cucumber/fluxoollamaassunto/etapas/A_Dado_que_o_usuario_esta_logado_no_sistema.java
@@ -1,25 +1,38 @@
 package org.coletivoJava.fw.projetos.erpColetivoJava.implementacao.cucumber.fluxoollamaassunto.etapas;
 
 import br.org.coletivoJava.fw.erp.implementacao.ia.model.usuarios.FabUsuarioPadrao;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import com.super_bits.modulosSB.SBCore.ConfigGeral.SBCore;
 import com.super_bits.modulosSB.SBCore.modulos.objetos.registro.Interfaces.basico.ItfUsuario;
 import cucumber.api.java.pt.Dado;
 import org.coletivoJava.fw.projetos.erpColetivoJava.api.EtapasFluxoOllamaAssunto;
+import org.coletivoJava.fw.projetos.erpColetivoJava.implementacao.FluxoAssuntoOllama;
 
 import static org.coletivoJava.fw.projetos.erpColetivoJava.implementacao.FluxoAssuntoOllama.USUARIO_AUTENTICADO;
 import static org.junit.Assert.assertTrue;
 
 public class A_Dado_que_o_usuario_esta_logado_no_sistema {
 
-	@Dado(EtapasFluxoOllamaAssunto.DADO_QUE_O_USUARIO_ESTA_LOGADO_NO_SISTEMA)
-	public void implementacaoEtapa() {
-		USUARIO_AUTENTICADO = (ItfUsuario) FabUsuarioPadrao.USUARIO_ATENDIMENTO.getRegistro();
+    @Dado(EtapasFluxoOllamaAssunto.DADO_QUE_O_USUARIO_ESTA_LOGADO_NO_SISTEMA)
+    public void implementacaoEtapa() {
+        USUARIO_AUTENTICADO = (ItfUsuario) FabUsuarioPadrao.USUARIO_ATENDIMENTO.getRegistro();
 
-		SBCore.getServicoSessao().logarEmailESenha(USUARIO_AUTENTICADO.getEmail(), USUARIO_AUTENTICADO.getSenha());
-		SBCore.getServicoSessao().getSessaoAtual().getUsuario();
-		SBCore.getServicoSessao().getSessaoAtual().isIdentificado();
+        SBCore.getServicoSessao().logarEmailESenha(USUARIO_AUTENTICADO.getEmail(), USUARIO_AUTENTICADO.getSenha());
+        SBCore.getServicoSessao().getSessaoAtual().getUsuario();
+        SBCore.getServicoSessao().getSessaoAtual().isIdentificado();
 
 
-		assertTrue("O usuario deve estar logado", SBCore.getServicoSessao().getSessaoAtual().isIdentificado());
-	}
+        String nomePersona = "IATeste";
+        String promptSystem = "Você é o **CasanovaIA**, assistente virtual da nossa empresa Casanova Digital especializado em atendimento ao cliente.\n";
+        String template = "chat";
+
+        Double temperature = 0.8;
+        boolean stream = false;
+        String quantize = "q4_0";
+
+        FluxoAssuntoOllama.OLLAMA.gerarPersona(nomePersona, promptSystem, template, temperature, stream, null);
+
+
+        assertTrue("O usuario deve estar logado", SBCore.getServicoSessao().getSessaoAtual().isIdentificado());
+    }
 }
